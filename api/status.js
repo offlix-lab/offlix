b8ccb10ed152d3e7897d3f044ce84821ed67c037
offlix-lab <offlixsupport@gmail.com>
2026-05-23 05:32:06 -0900

    status js changed

--- api/status.js
+++ api/status.js
@@ -2,23 +2,38 @@ export default async function handler(req, res) {
 
     try {
 
+        const formData = new URLSearchParams();
+
+        formData.append("sh_st", req.body.sh_st);
+        formData.append("grade", req.body.grade);
+        formData.append("section", req.body.section);
+        formData.append("sex", req.body.sex);
+        formData.append("semister", req.body.semister);
+        formData.append("limit", req.body.limit);
+
         const response = await fetch(
             "http://offlix.atwebpages.com/src/b/sh_st.php",
             {
                 method: "POST",
-                body: req.body
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: formData
             }
         );
 
-        const data = await response.json();
+        const text = await response.text();
 
-        res.status(200).json(data);
+        console.log(text);
+
+        res.status(200).send(text);
 
     } catch (error) {
+
+        console.error(error);
 
         res.status(500).json({
             error: error.message
         });
-
     }
 }
