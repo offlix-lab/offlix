597023c05bbcfbda1323b71270763007d46cf7c0
offlix-lab <offlixsupport@gmail.com>
2026-05-23 05:30:09 -0900

    status js changed

--- api/status.js
+++ api/status.js
@@ -1,16 +1,24 @@
 export default async function handler(req, res) {
-  try {
-    const response = await fetch(
-      "http://offlix.atwebpages.com/src/b/sh_st.php"
-    );
 
-    const data = await response.text();
+    try {
 
-    res.status(200).send(data);
+        const response = await fetch(
+            "http://offlix.atwebpages.com/src/b/sh_st.php",
+            {
+                method: "POST",
+                body: req.body
+            }
+        );
 
-  } catch (error) {
-    res.status(500).json({
-      error: error.message
-    });
-  }
+        const data = await response.json();
+
+        res.status(200).json(data);
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+
+    }
 }
